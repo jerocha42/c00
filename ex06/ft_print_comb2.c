d70b3385e8d1c03a7fe1a8ab708e4b1ebe7aacd1
jerocha42 <jerocha-@student.42sp.org.br>
2026-04-12 16:16:40 -0300

    temp ex06

---
+++ ex06/ft_print_comb2.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jerocha- <jerocha-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/10 20:36:10 by jerocha-          #+#    #+#             */
+/*   Updated: 2026/04/11 20:18:19 by jerocha-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_print_comb2(void);
+
+void	ft_print_comb2(void)
+{
+	// Diferentes combinações de dois números de dois dígitos
+	// Os 2 números do par precisam ser distintos entre si
+	// Os pares não podem se repetir
+	// 00 01 - 00 02 - 00 03 - 00 04 - 00 99
+	// 01 02 - 01 03 - 01 04 - 01 98 - 01 99
+	// 02 03 - 02 04 - 02 05 - 02 98 - 02 99
+	// 97 98 - 97 99 
+	// 98 99
+	// Dois loops
+		// Começa em 0
+}
+
+int	main(void)
+{
+	return (0);
+}
